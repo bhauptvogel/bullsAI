9068265e65f45bb4a64dad89f70f8a1d9f2b3d8b
bhauptvogel <b.vah317@gmail.com>
2024-01-31 20:16:38 +0100

    result of play game automatically in schedule csv

--- fantasy/play_day.py
+++ fantasy/play_day.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import argparse
 import uuid
-from bullsai.game_sim import handle_game
+from bullsai.game_sim import handle_game, Game
 
 def parse_args():
     parser=argparse.ArgumentParser()
@@ -14,9 +14,24 @@ def get_league_of_player() -> int:
     players = pd.read_csv('fantasy/players.csv')
     return int(players[players['Name'] == 'PLAYER']['League'].values[0])
 
-def get_player_average(name: str) -> float:
+def get_opponent_average(name: str) -> float:
     players = pd.read_csv('fantasy/players.csv')
     return float(players[players['Name'] == name]['AVG'].values[0])
+
+def put_result_in_csv(player_legs_won: int, opponent_legs_won: int, game_id: str, home_player: str, opponent_name: str):
+    league_schedule_csv = f'fantasy/schedule_league_{get_league_of_player()}.csv'
+    league_schedule_df = pd.read_csv(league_schedule_csv)
+
+    if home_player == 'player':
+        result = f'{player_legs_won}-{opponent_legs_won}'
+        league_schedule_df.loc[(league_schedule_df['Home Player'] == 'PLAYER') & (league_schedule_df['Away Player'] == opponent_name), 'Result'] = result
+        league_schedule_df.loc[(league_schedule_df['Home Player'] == 'PLAYER') & (league_schedule_df['Away Player'] == opponent_name), 'Game Log'] = game_id
+    else:
+        result = f'{opponent_legs_won}-{player_legs_won}'
+        league_schedule_df.loc[(league_schedule_df['Away Player'] == 'PLAYER') & (league_schedule_df['Home Player'] == opponent_name), 'Result'] = result
+        league_schedule_df.loc[(league_schedule_df['Away Player'] == 'PLAYER') & (league_schedule_df['Home Player'] == opponent_name), 'Game Log'] = game_id
+    
+    league_schedule_df.to_csv(league_schedule_csv, index=False)
 
 def play(day: int):
     league_schedule = pd.read_csv(f'fantasy/schedule_league_{get_league_of_player()}.csv')
@@ -29,7 +44,8 @@ def play(day: int):
     first_to_throw = 'player' if game_to_play['Home Player'].values[0] == 'PLAYER' else 'bot'
 
     game_id = str(uuid.uuid4())
-    handle_game(game_id=game_id, bot_average=get_player_average(opponent), sets_to_win=1, legs_to_win=5, first_to_throw=first_to_throw, starting_points=501, bot_name=opponent, save_log_location='fantasy/game_logs/')
+    result = handle_game(game_id=game_id, bot_average=get_opponent_average(opponent), sets_to_win=1, legs_to_win=5, first_to_throw=first_to_throw, starting_points=51, bot_name=opponent, save_log_location='fantasy/game_logs/')
+    put_result_in_csv(result.player_legs_won, result.bot_legs_won, game_id, first_to_throw, opponent)
 
 if __name__ == '__main__':
     inputs=parse_args()
