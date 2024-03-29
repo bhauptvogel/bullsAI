fc15bcc344a44e3a92eb9cb48eae293d05b738d3
bhauptvogel <b.vah317@gmail.com>
2024-03-29 18:51:24 +0100

    cup update

--- fantasy/cup/round.py
+++ fantasy/cup/round.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import uuid
+import random
+from bullsai.game_sim import handle_game
 from bullsai import dart
 from bullsai import empirical_std
 
@@ -60,7 +63,7 @@ def show():
 
         print(f'Match {i+1}: {player1} vs {player2}')
 
-def simulate():
+def simulate(round: int):
     # TODO: Only take players that have not lost yet (from results.csv)
     print("Simulating...")
     players = pd.read_csv(f'fantasy/cup/cup_players.csv')
@@ -73,48 +76,30 @@ def simulate():
         if player1 != 'PLAYER' and player2 != 'PLAYER':
             print(f"Simulating Match {i+1}: {player1} vs {player2}")
             
-            GAMES_TO_WIN = 3
-            player1_game_wins = 0
-            player2_game_wins = 0
 
             player1_avg = get_player_average(player1)
             player2_avg = get_player_average(player2)
 
-            while player1_game_wins < GAMES_TO_WIN and player2_game_wins < GAMES_TO_WIN:
+            player1_leg_wins = 0
+            player2_leg_wins = 0
+
+            LEGS_TO_WIN = 3 + round
+            player_beginning = random.randint(1, 2)
+
+            while player1_leg_wins < LEGS_TO_WIN and player2_leg_wins < LEGS_TO_WIN:
+                leg_winner = sim_leg(player1_avg, player2_avg, player_beginning, 501)
+                if leg_winner == 1:
+                    player1_leg_wins += 1
+                elif leg_winner == 2:
+                    player2_leg_wins += 1
+                player_beginning = 1 if player_beginning == 2 else 2
+
+            print(f"Match Result: {player1_leg_wins} - {player2_leg_wins}")
                 
-                game_winner = 0
+            winner = player1 if player1_leg_wins > player2_leg_wins else player2
+            loser = player1 if player1_leg_wins < player2_leg_wins else player2
 
-                player1_leg_wins = 0
-                player2_leg_wins = 0
-
-                LEGS_TO_WIN = 5
-                player_beginning = (player1_game_wins + player2_game_wins + 1) % 2 + 1
-
-                while player1_leg_wins < LEGS_TO_WIN and player2_leg_wins < LEGS_TO_WIN:
-                    leg_winner = sim_leg(player1_avg, player2_avg, player_beginning, 501)
-                    if leg_winner == 1:
-                        player1_leg_wins += 1
-                    elif leg_winner == 2:
-                        player2_leg_wins += 1
-                    player_beginning = 1 if player_beginning == 2 else 2
-
-                game_winner = 1 if player1_leg_wins > player2_leg_wins else 2
-
-                if game_winner == 1:
-                    player1_game_wins += 1
-                elif game_winner == 2:
-                    player2_game_wins += 1
-                else:
-                    raise ValueError("Game winner is not valid")
-                
-                print(f"Game {player1_game_wins + player2_game_wins}: {player1} {player1_game_wins} - {player2_game_wins} {player2}")
-
-
-            winner = player1 if player1_game_wins > player2_game_wins else player2
-            loser = player1 if player1_game_wins < player2_game_wins else player2
-
-
-            results = pd.concat([results, pd.DataFrame([[player1, player2, f'{player1_game_wins} - {player2_game_wins}']], columns=['Player 1', 'Player 2', 'Result'])])
+            results = pd.concat([results, pd.DataFrame([[player1, player2, f'{player1_leg_wins} - {player2_leg_wins}']], columns=['Player 1', 'Player 2', 'Result'])])
 
             print(f"Adding {winner} to the next round.")
             print(f"Removing {loser} from the next round.")
@@ -125,9 +110,7 @@ def simulate():
     results.to_csv(f'fantasy/cup/results.csv', index=False)
 
 
-def play(game_number: int = 1):
-    from bullsai.game_sim import handle_game
-    import uuid
+def play(round: int):
     print("Playing...")
 
     players = pd.read_csv(f'fantasy/cup/cup_players.csv')
@@ -142,15 +125,16 @@ def play(game_number: int = 1):
             game_id = str(uuid.uuid4())
             opponent_average = get_player_average(player1) if player2 == 'PLAYER' else get_player_average(player2)
             opponent_name = player1 if player2 == 'PLAYER' else player2
-            first_to_throw = 'player' if game_number % 2 == 1 else 'bot'
-            result = handle_game(game_id=game_id, bot_average=opponent_average, sets_to_win=1, legs_to_win=5, first_to_throw=first_to_throw, starting_points=501, bot_name=opponent_name, save_log_location='fantasy/cup/game_logs/')
+            first_to_throw = random.randint(1, 2)
+            legs_to_win = 3 + round
+            result = handle_game(game_id=game_id, bot_average=opponent_average, sets_to_win=1, legs_to_win=legs_to_win, first_to_throw=first_to_throw, starting_points=501, bot_name=opponent_name, save_log_location='fantasy/cup/game_logs/')
 
 def main():
     parser = argparse.ArgumentParser(description="Process some commands.")
     parser.add_argument("--show", action="store_true", help="Execute the show command")
     parser.add_argument("--simulate", action="store_true", help="Execute the simulate command")
     parser.add_argument("--play", action="store_true", help="Execute the play command")
-    parser.add_argument("--game_number", type=int, default=1, help="The game number to play")
+    parser.add_argument("--round", default=1, type=int, help="Specify the round number")
 
     args = parser.parse_args()
 
@@ -161,9 +145,9 @@ def main():
         if args.show:
             show()
         if args.simulate:
-            simulate()
+            simulate(args.round)
         if args.play:
-            play(args.game_number)
+            play(args.round)
 
 if __name__ == "__main__":
     main()
